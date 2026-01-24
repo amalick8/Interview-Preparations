abbbbc619356ff36519aac48a8b5c871c7daf513
Ammar Malick <ammarmalick2006@gmail.com>
2026-01-23 23:49:35 -0600

    Add initial notes on string characteristics and behavior

---
+++ day2_dsa_notes.py
@@ -0,0 +1,5 @@
+# A string is a sequence of characters that are stored in order and you can loop
+# through it one character at a time
+
+# Strings are ordered and each character can be accessed one by one, also length matters
+
