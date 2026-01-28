91bfab42031435f17b7e40b4e8835692f6791450
Ammar Malick <ammarmalick2006@gmail.com>
2026-01-27 23:34:04 -0600

    Add initial notes on two pointers technique in day5_dsa_notse.py

---
+++ day5_dsa_notse.py
@@ -0,0 +1,10 @@
+# A pointer is just a variable that points to a position in something, usually:
+# A string
+# An array
+# A list
+
+# Two pointers = using TWO positions at the same time
+# Usually one starts on the left
+# One starts on the right
+# They move towards each other
+
