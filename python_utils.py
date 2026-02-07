56b3ef2f437db3fd5d839412b89d88909e71bc09
Ammar Malick <ammarmalick2006@gmail.com>
2026-02-06 19:06:54 -0600

    Add merge_sorted_lists function implementation and its test to python_utils.py

--- python_utils.py
+++ python_utils.py
@@ -146,6 +146,52 @@ class ListNode:
         self.val = val
         self.next = next
 
+def merge_sorted_lists(l1,l2):
+    dummy = ListNode()
+    current = dummy
+
+    while l1 and l2:
+        if l1.val <= l2.val:
+            current.next = l1
+            l1 = l1.next
+        else:
+            current.next = l2
+            l2 = l2.next
+        current = current.next
+        
+    if l1:
+            current.next = l1
+    else:
+            current.next = l2
+        
+    return dummy.next
+
+def test_merge_sorted_lists():
+    a1 = ListNode(1)
+    a2 = ListNode(2)
+    a3 = ListNode(3)
+
+    a1.next = a2
+    a2.next = a3
+
+    b1 = ListNode(1)
+    b2 = ListNode(3)
+    b3 = ListNode(4)
+
+    b1.next = b2
+    b2.next = b3
+
+    merged = merge_sorted_lists(a1,b1)
+
+
+    assert merged.val == 1
+    assert merged.next.val == 1
+    assert merged.next.next.val == 2
+    assert merged.next.next.next.val == 3
+    assert merged.next.next.next.next.val == 4
+    assert merged.next.next.next.next.next.val == 4
+    assert merged.next.next.next.next.next.next is None
+
 def reverse_linked_list(head):
     if head == None:
         return None
