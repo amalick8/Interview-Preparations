b4f1d12284d47460b29b5418114a866ee5b4e033
Ammar Malick <ammarmalick2006@gmail.com>
2026-02-07 00:01:28 -0600

    Refactor comments and enhance queue explanations in day9_dsa_notes.py

--- day9_dsa_notes.py
+++ day9_dsa_notes.py
@@ -23,7 +23,8 @@ def merged_sorted_list(l1,l2):
 
     return dummy.next
 
-# Stacks
+# STACKS
+
 # A stack is a container where:
 # The last thing you put is the first thing you take out
 # Last in, First out
@@ -55,4 +56,49 @@ def merged_sorted_list(l1,l2):
 # Expression evaluation
 # Parantheses check
 # Function calls (call stack)
+
+# QUEUES
+
+# A queue is a container where:
+# The first thing you put in is the first you take out
+# The rule is called:
+# FIFO - First in, First out
+
+# Think of a line at a store:
+# First person in line > served first
+# New people go to the back
+# You can't skip the line
+
+# Operations of a Queue
+# Enqueue > add to back
+# Dequeue > remove from front
+# Front/Peek > Look at first element
+# Is empty?
+
+# Stack - LIFO - Plates
+# Queue - FIFO - Line
+
+# Fastest way
+# from collections import deque
+# queue = deque()
+# queue.append(x) # equeue
+# queue.popleft() # dequeue
+
+# Queues are used when order of arrival matters:
+# Task scheduling
+# Pring jobs
+# BFS (graph traversal)
+# Customer service systems
+
+def merge(nums1, m, nums2, n):
+    for num in range(len(nums1)-1):
+        if nums1[num] == 0:
+            nums1.pop(num)
+    for num in range(len(nums2)-1):
+        if nums2[num] == 0:
+            nums2.pop(num)
+    
+    new_list = nums1 + nums2
+    new_list.sort()
+    return new_list
 
