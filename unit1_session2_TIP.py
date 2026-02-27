2ba8e23948f8980b424674796b8385b8e21def75
Ammar Malick <ammarmalick2006@gmail.com>
2026-02-26 20:58:38 -0600

    Update unit1 session 2 notes

--- unit1_session2_TIP.py
+++ unit1_session2_TIP.py
@@ -40,22 +40,65 @@ def max_difference(lst):
     return lst[-1]-lst[0]
 print(max_difference([1,2,3,4,5]))
 
+def reverse_list(lst):
+    new_list = []
+    first1 = len(lst)-1
+    while first1 >= 0 and first1<len(lst):
+        new_list.append(lst[first1])
+        first1 -=1
+    return new_list
+print(reverse_list([1,2,3,4,5]))
+
+def reverse_list1(lst):
+    new_list = []
+    for i in range(len(lst)-1,-1,-1):
+        new_list.append(lst[i])
+    return new_list
+print(reverse_list1([1,2,3,4,5]))
+
+def get_evens(lst):
+    new_list = []
+    for num in lst:
+        if num % 2 == 0:
+            new_list.append(num)
+    return new_list
+print(get_evens([1,2,3,4,5]))
+
+def multiplication_table(num):
+    for i in range(1,11):
+        print(num*i)
+multiplication_table(7)
+
+def find_divisors(n):
+    new_list = []
+    for i in range(1,n+1):
+        if n%i == 0:
+            new_list.append(i)
+    return new_list
+print(find_divisors(6))
 
 
 
-   
-            
+def move_zeroes(nums):
+# sure
+# www.linkedin.com/in/ammar-malick-1023b9278
+# you see it on your end? ya kk add me. on i t
+    # can you put your linkedin here please?
+    # havent seen an invite
+    # give me yours
+    # your good lol
+    # give me a min 
 
-
-
-
-
-
-
-        
-
+    # I am new to this l
     
 
-
-
-
+    # okay i gotchu. 
+    # do you have a linkedin account 
+    # I think I just followed you on it 
+    # click the 3 dots and lcick connect
+    # that way i follow you back 
+    # and you build connections which is what matters
+    # thank you so much 
+    # of course i got it. 
+    # always make sure to click connect ok? will do 
+            
