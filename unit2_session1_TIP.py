2ac06d49989f711a1cf50f6c451d28fcfac01c83
Ammar Malick <ammarmalick2006@gmail.com>
2026-03-03 20:25:22 -0600

    Refactor all_in function to simplify logic and improve readability

--- unit2_session1_TIP.py
+++ unit2_session1_TIP.py
@@ -1,3 +1,4 @@
+#Problem1
 #check whether all elements in a are in b
 ##iterate through a and check for each element in a if this is present in b
 ###if all are present, return True
@@ -6,16 +7,28 @@
 def all_in(a, b):
     if len(a)>len(b):
         return False
-
-    counter = 0
     for i in a:
-        if i in b:
-            counter += 1
-        if counter == len(a):
-            return True
-        else:
+        if not i in b:
             return False
+        else:
+            return True
 lst_1 = [1, 2]
 lst_2 = [1, 2, 3]
 print(all_in(lst_1, lst_2))
 print(all_in(lst_2, lst_1))
+
+#Problem2
+#create a dictionary to store key value pairs
+#the parameters are going to be in list format because there are indices
+#dictionary_name[key] = value
+def create_dictionary(keys,values):
+    new_dictionary = {}
+    for i in range(0,len(keys)):
+        new_dictionary[keys[i]] = values[i]
+    return new_dictionary
+keys = ['peanut', 'dragon', 'star', 'pop', 'space']
+values = ['butter', 'fly', 'fish', 'corn', 'ship']
+print(create_dictionary(keys,values))
+
+
+#Problem3
