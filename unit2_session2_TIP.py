5fc866082d2759075477eb1e5fb67a733362cfb1
Ammar Malick <ammarmalick2006@gmail.com>
2026-03-05 19:21:03 -0600

    Add count_by_category function to group and count items by category

---
+++ unit2_session2_TIP.py
@@ -0,0 +1,18 @@
+'''
+Input: tuples, always same size, 0 category, 1 value (item)
+Output: dictionary, keys, categories, values will be count
+Logic: want to group and count by category
+'''
+
+# Plan (hint: how do we use dictionaries to group things together)
+
+'''
+1. Initialize our count_dictionary {}
+2. In a foor loop we check if there is a new count
+3. If so we will intialize
+4. If not we will increment the existing count
+'''
+
+def count_by_category(items):
+    count_dictionary = {} # starts empty, keys: category, values: count #
+
