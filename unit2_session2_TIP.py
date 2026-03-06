dec65ec0737a97fcc4d233617548d490e797e326
Ammar Malick <ammarmalick2006@gmail.com>
2026-03-05 20:55:15 -0600

    Refactor problem statement for counting occurrences function and remove unnecessary docstring

--- unit2_session2_TIP.py
+++ unit2_session2_TIP.py
@@ -61,11 +61,11 @@ dict1 = {"a": 1, "b": 2, "c": 3}
 dict2 = {"b": 4, "c": 5, "d": 6}
 common_list = common_keys(dict1, dict2)
 print(common_list)
-'''
+
 
 print("This is problem 3 here")
 
-'''Write a function that takes in a list of integers nums
+Write a function that takes in a list of integers nums
 and counts the number of occurrences of each integer. 
 The function returns the result as a dictionary with integers as keys and their counts as values.'''
 
