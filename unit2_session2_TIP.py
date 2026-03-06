1abfe811c8f98511a6612252ac9cdecd47849177
Ammar Malick <ammarmalick2006@gmail.com>
2026-03-05 20:11:23 -0600

    Refactor count_by_category function and add additional problem statements with corresponding functions

--- unit2_session2_TIP.py
+++ unit2_session2_TIP.py
@@ -13,7 +13,7 @@ Logic: want to group and count by category
 4. If not we will increment the existing count
 '''
 
-def count_by_category(items):
+'''def count_by_category(items):
     count_dictionary = {} # starts empty, keys: category, values: count #
 
     for item in items:
@@ -29,7 +29,40 @@ def count_by_category(items):
     return count_dictionary
 
 items = [("fruits", "apple"), ("vegetables", "carrot"), ("fruits", "banana")]
-print(count_by_category(items))
+print(count_by_category(items))'''
+
+
+'''print("Problem 1 here")
+def cast_vote(votes, candidate): 
+    if candidate not in votes:
+        votes[candidate] = 1
+    else:
+        votes[candidate] += 1
+    return votes
+votes = {"Alice": 5, "Bob": 3}
+print(cast_vote(votes, "Gina"))'''
+
+'''
+print('Problem 2 here')
+# 1. We will be provided 2 dictionaries
+# 2. Whichever keys are common in the dictionaries, will be outputted
+
+# Steps
+# Iterate through one dictionary using the other dictionaries key
+# Return if key found in secondary dictionary
+
+def common_keys(dict1, dict2):
+    common_key_types = []
+    for key in dict1:
+        if key in dict2:
+            common_key_types.append(key) 
+    return common_key_types
+dict1 = {"a": 1, "b": 2, "c": 3}
+dict2 = {"b": 4, "c": 5, "d": 6}
+common_list = common_keys(dict1, dict2)
+print(common_list)
+'''
+
 
 
 
