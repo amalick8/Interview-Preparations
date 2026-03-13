e67723c8fd6da3e51fbcbb947214f2a1e43c0c23
Muhammad Ammar Malick <ammarmalick2006@gmail.com>
2026-03-12 19:18:34 -0500

    Add initial implementation of unit3_session2_TIP.py with string operations

---
+++ unit3_session2_TIP.py
@@ -0,0 +1,12 @@
+# String are sequences of characters.
+# Common operations: conatenation, slicing, and formatting
+# Useful methods: .lower(), .upper(), .split(), .replace()
+
+s = 'Hello World'
+print(s.lower()) # 'hello world'
+print(s.upper()) # 'hello world'
+
+userName = 'Ammar2026'
+currentTime = 'March 12, 7:10pm'
+
+print(f'{userName} has signed in at {currentTime}')
